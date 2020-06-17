0bcc7eb562387403b8202e3e466661802bc72584
Zhou Nan <nzhou002@gmail.com>
2020-06-17 10:33:58 +0800

    Redux Thunk

--- course-2/confusion/src/components/MainComponent.js
+++ course-2/confusion/src/components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
+
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
@@ -8,7 +10,8 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
-import { addComment } from '../redux/ActionCreators';
+
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -20,7 +23,9 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes()) },
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
 });
 
 class Main extends Component {
@@ -29,7 +34,9 @@ class Main extends Component {
     super(props);
   }
 
-
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
 
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId });
@@ -38,20 +45,24 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return(
-        <Home
-            dish={ this.props.dishes.filter((dish) => dish.featured)[0] }
-            promotion={ this.props.promotions.filter((promo) => promo.featured)[0] }
-            leader={ this.props.leaders.filter((leader) => leader.featured)[0] }
-        />
+          <Home
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
-    };
+    }
 
-    const DishWithId = ({ match }) => {
+    const DishWithId = ({match}) => {
       return(
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-          addComment={this.props.addComment}
-        />
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment}
+          />
       );
     };
 
@@ -63,7 +74,7 @@ class Main extends Component {
             <Route exact path='/menu' component={ () => <Menu dishes={ this.props.dishes } /> } />
             <Route path='/menu/:dishId' component={ DishWithId } />
             <Route exact path='/aboutus' component = { () => <About leaders={ this.props.leaders } /> } />
-            <Route exact path='/contactus' component={ Contact } />} />
+            <Route exact path='/contactus' component={ () => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
             <Redirect to="/home" />
           </Switch>
         <Footer />
--- course-2/confusion/src/redux/configureStore.js
+++ course-2/confusion/src/redux/configureStore.js
@@ -1,8 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { Dishes } from './Dishes';
 import { Comments } from './Comments';
 import { Promotions } from './Promotions';
 import { Leaders } from './Leaders';
+import { InitialFeedback } from './Forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -10,8 +14,12 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
-        })
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
