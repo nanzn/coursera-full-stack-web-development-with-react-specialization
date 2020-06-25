98f741e26105c820189d2c7d74dbcaef53777275
Zhou Nan <nzhou002@gmail.com>
2020-06-26 07:52:08 +0800

    cors

--- course-4/conFusionServer/routes/users.js
+++ course-4/conFusionServer/routes/users.js
@@ -3,6 +3,7 @@ var passport = require('passport');
 const bodyParser = require('body-parser');
 
 var authenticate = require('../authenticate');
+const cors = require('./cors');
 var User = require('../models/user');
 
 var router = express.Router();
@@ -11,7 +12,8 @@ router.use(bodyParser.json());
 
 /* GET users listing. */
 router.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     User.find({})
     .then((users) => {
         res.statusCode = 200;
@@ -21,7 +23,7 @@ router.route('/')
     .catch((err) => next(err));
 })
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}),
     req.body.password, (err, user) => {
     if(err) {
@@ -52,14 +54,14 @@ router.post('/signup', (req, res, next) => {
 });
 
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
